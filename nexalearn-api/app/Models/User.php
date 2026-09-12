8ec29af59aa7ece2090a149db56614baf76523e8
josemanuelbt703-crypto <josemanuelbt703@gmail.com>
2026-09-12 14:23:34 -0500

    Configure Sanctum
    Configure Sanctum authentication

--- nexalearn-api/app/Models/User.php
+++ nexalearn-api/app/Models/User.php
@@ -7,6 +7,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -22,6 +23,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
